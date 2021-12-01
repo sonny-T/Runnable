17f357dd99a3b317d4846cc6dda96fcb47a6dd2f
sonnylier <sonny_lier@163.com>
2021-12-01 22:00:29 +0800

    set entry address

--- tools/revng-lift/CodeGenerator.cpp
+++ tools/revng-lift/CodeGenerator.cpp
@@ -753,12 +753,11 @@ void CodeGenerator::translate(uint64_t VirtualAddress) {
   // Create an instance of JumpTargetManager
   JumpTargetManager JumpTargets(MainFunction, PCReg, Binary);
 
-//  if (VirtualAddress == 0) {
-//    outs()<<"testetst\n";
-//    JumpTargets.harvestGlobalData();
-//    VirtualAddress = Binary.entryPoint();
-//  }
-  VirtualAddress = Binary.entryPoint();
+  if (VirtualAddress == 0) {
+    //JumpTargets.harvestGlobalData();
+    VirtualAddress = Binary.entryPoint();
+  }
+//  VirtualAddress = Binary.entryPoint();
   JumpTargets.registerJT(VirtualAddress, JTReason::GlobalData);
 
   // Initialize the program counter
